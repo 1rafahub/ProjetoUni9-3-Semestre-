acc142c6d2ecb18a4f976140c7c807e6def1f538
1rafahub <1rafaelsilvalima@gmail.com>
2022-10-11 19:53:54 -0300

    adicionando views

--- controllers/routes.py
+++ controllers/routes.py
@@ -5,7 +5,7 @@ import requests
 
 
 @app.route("/home", methods = ['GET'])
-def users():
+def home():
 
     dados = []
     conn = connection_db()
@@ -24,4 +24,25 @@ def users():
     conn.close()
 
     return render_template("./home/home.html", dados = dados)
+
+@app.route("/contratarservicos", methods = ['GET'])
+def contratar_servicos():
+
+    servicos = []
+    conn = connection_db()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM dbo.tb_user_services")
+
+    for row in cursor.fetchall():
+        servicos.append({
+                        "id_user_service": row[0],
+                        "id_user": row[1],
+                        "id_type_service" : row[2],
+                        "activities": row[3],
+                        "hour_value" : row[4]
+                        })
+
+    conn.close()
+
+    return render_template("./contratar_servicos/contratar_servicos.html", servicos = servicos)
 
